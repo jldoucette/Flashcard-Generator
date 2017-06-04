3c9291472bad9316bfc2a156996629ed462b53fd
jldoucette <jd@jonathandoucette.com>
2017-06-03 21:38:22 -0700

    with Inquirer

--- Cards.js
+++ Cards.js
@@ -1,19 +1,48 @@
+var inquirer = require("inquirer");
 var BasicCard=require("./BasicCard");
 var ClozeCard=require("./ClozeCard");
 var type=process.argv[2];
+
 if (type=="basic") {
-var question=process.argv[3];
-var answer=process.argv[4];
-var basictest= BasicCard(question, answer);
-console.log("Front Text: "+basictest.front)
-console.log("Back Text: "+basictest.back);
+    askBasicQuestions();
 }
+
 if (type=="cloze") {
-var question=process.argv[3];
-var cloze=process.argv[4];
-var clozetest=ClozeCard(question,cloze);
-console.log(clozetest);
-console.log("Full Text: "+clozetest.fullText);
-console.log("Cloze Text: "+clozetest.cloze);
-console.log("Partial Text: "+clozetest.partial);
+    askClozeQuestions();
+}
+
+function askBasicQuestions() {
+    inquirer.prompt([
+        {
+            name: "question",
+            message: "What is the question to ask? "
+        }, {
+            name: "answer",
+            message: "What is the correct answer to the question? "
+        }
+        
+        ]).then(function(basic) {
+            var basictest= BasicCard(basic.question,basic.answer);
+            console.log("Front Text: "+basictest.front)
+            console.log("Back Text: "+basictest.back);
+        });
+    }
+
+function askClozeQuestions() {
+    inquirer.prompt([
+        {
+            name: "question",
+            message: "What is the full text of the statement? "
+        }, {
+            name: "cloze",
+            message: "What is the Cloze phrase to remove from the statement? "
+        }
+        
+        ]).then(function(clozeformat) {
+            var clozetest= ClozeCard(clozeformat.question,clozeformat.cloze);
+            console.log(clozetest);
+            console.log("Full Text: "+clozetest.fullText);
+            console.log("Cloze Text: "+clozetest.cloze);
+            console.log("Partial Text: "+clozetest.partial);
+        });
 }
