c1fe8e20d898ee714234a1af4a9f3d2afbc870b5
jldoucette <jd@jonathandoucette.com>
2017-06-03 21:02:36 -0700

    new not required

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,12 +1,18 @@
 var ClozeCard = function(text,cloze) {
-    this.fullText=text;
-    this.cloze=cloze;
-    this.partial=text.replace(cloze," ...");
-    if (this.partial==this.fullText){
+    if (this instanceof ClozeCard) {
+        this.fullText=text;
+        this.cloze=cloze;
+        this.partial=text.replace(cloze," ...");
+        if (this.partial==this.fullText){
         console.log("*************Oops. Your cloze deletion does not appear in your Text Entry************");
-    }
-    else if (this.partial != this.fullText) {
+        }
+        else if (this.partial != this.fullText) {
         console.log("Your cloze deletion was successful");
+        }
+    }
+    else {
+        return new ClozeCard(text,cloze);
     }
 };
+
 module.exports=ClozeCard;
