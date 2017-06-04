c8e4977cff5f254f0046044500706315b27417fa
jldoucette <jd@jonathandoucette.com>
2017-06-03 22:23:23 -0700

    Added logging files

--- Cards.js
+++ Cards.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
 var BasicCard=require("./BasicCard");
 var ClozeCard=require("./ClozeCard");
+var fs=require('fs');
 var type=process.argv[2];
 
 if (type=="basic") {
@@ -22,9 +23,12 @@ function askBasicQuestions() {
         }
         
         ]).then(function(basic) {
-            var basictest= BasicCard(basic.question,basic.answer);
-            console.log("Front Text: "+basictest.front)
-            console.log("Back Text: "+basictest.back);
+            var basicentry= BasicCard(basic.question,basic.answer);
+            console.log("Front Text: "+basicentry.front);
+            console.log("Back Text: "+basicentry.back);
+            var logBasicQuestion = "\n***Question: " + basicentry.front +
+    " ***Answer: " + basicentry.back;
+    fs.appendFile("basic.txt", logBasicQuestion);
         });
     }
 
@@ -39,10 +43,16 @@ function askClozeQuestions() {
         }
         
         ]).then(function(clozeformat) {
-            var clozetest= ClozeCard(clozeformat.question,clozeformat.cloze);
-            console.log(clozetest);
-            console.log("Full Text: "+clozetest.fullText);
-            console.log("Cloze Text: "+clozetest.cloze);
-            console.log("Partial Text: "+clozetest.partial);
+            var clozeentry= ClozeCard(clozeformat.question,clozeformat.cloze);
+            console.log(clozeentry);
+            console.log("Full Text: "+clozeentry.fullText);
+            console.log("Cloze Text: "+clozeentry.cloze);
+            console.log("Partial Text: "+clozeentry.partial);
+            console.log("Is Valid Statement: "+clozeentry.validStatement);
+            if (clozeentry.validStatement) {
+                var logClozeQuestion = "\n***Full Text: " + clozeentry.fullText +
+                " ***Cloze text: " + clozeentry.cloze + " ***Partial Text: "+ clozeentry.partial;
+                fs.appendFile("cloze.txt", logClozeQuestion);
+            }
         });
 }
--- ClozeCard.js
+++ ClozeCard.js
@@ -5,9 +5,11 @@ var ClozeCard = function(text,cloze) {
         this.partial=text.replace(cloze," ...");
         if (this.partial==this.fullText){
         console.log("*************Oops. Your cloze deletion does not appear in your Text Entry************");
+        this.validStatement=false;
         }
         else if (this.partial != this.fullText) {
         console.log("Your cloze deletion was successful");
+        this.validStatement=true;
         }
     }
     else {
